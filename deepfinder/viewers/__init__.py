68e554f9dad1414dc11255ba870685145890b969
emoebel <emmanuel.moebel@inria.fr>
2021-02-18 14:14:46 +0100

    added viewer template

---
+++ deepfinder/viewers/__init__.py
@@ -0,0 +1,1 @@
+from .viewer_deepfinder import DeepFinderViewer
---
+++ deepfinder/viewers/viewer_deepfinder.py
@@ -0,0 +1,30 @@
+import pyworkflow.viewer as pwviewer
+import tomo.objects
+from deepfinder.protocols import DeepFinderSegment
+
+class DeepFinderViewer(pwviewer.Viewer):
+    _label = 'Ortho-slice volume explorer'
+    _targets = [
+        tomo.objects.Tomogram,
+        tomo.objects.TomoMask,
+        tomo.objects.SetOfTomograms,
+        tomo.objects.SetOfTomoMasks,
+        DeepFinderSegment
+    ]
+
+    def _visualize(self, obj, **kwargs):
+
+        # deepfinder_args = ''
+        # if self.tomogram.get() != None:
+        #     fname = self.tomogram.get().getFileName()
+        #     deepfinder_args += '-t ' + fname
+        # if self.segmentation.get() != None:
+        #     for seg in self.segmentation.get().iterItems():
+        #         fname = seg.getFileName()
+        #         deepfinder_args += ' -l ' + fname
+        #
+        #         fname_tomo = str(seg.getTomoName())
+        #         if fname_tomo != '':  # if a tomo is linked to segmentation, also display tomo
+        #             deepfinder_args += ' -t ' + fname_tomo
+
+        pass
