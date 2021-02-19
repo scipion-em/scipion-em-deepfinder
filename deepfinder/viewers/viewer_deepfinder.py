aef9bafbaaceae503f4320367a73e21a845aff17
emoebel <emmanuel.moebel@inria.fr>
2021-02-19 15:29:36 +0100

    viewer_deepfinder: started developing _visualize method. Added classes DeepFinderTomoView and DeepFinderSetOfTomogramsView

--- deepfinder/viewers/viewer_deepfinder.py
+++ deepfinder/viewers/viewer_deepfinder.py
@@ -3,19 +3,29 @@ import tomo.objects
 from deepfinder.protocols import DeepFinderSegment, DeepFinderGenerateTrainingTargetsSpheres
 from tomo.protocols import ProtImportTomograms
 
+from deepfinder import Plugin
+
 class DeepFinderViewer(pwviewer.Viewer):
     _label = 'Ortho-slice volume explorer'
     _targets = [
         tomo.objects.Tomogram,
         tomo.objects.TomoMask,
         tomo.objects.SetOfTomograms,
-        tomo.objects.SetOfTomoMasks,
-        ProtImportTomograms,
-        DeepFinderGenerateTrainingTargetsSpheres,
-        DeepFinderSegment
+        tomo.objects.SetOfTomoMasks#,
+        #ProtImportTomograms,
+        #DeepFinderGenerateTrainingTargetsSpheres,
+        #DeepFinderSegment
     ]
 
     def _visualize(self, obj, **kwargs):
+        env = Plugin.getEnviron()
+        view = []
+        cls = type(obj)
+
+        if issubclass(cls, tomo.objects.Tomogram):
+            view = DeepFinderTomoView(obj)
+        elif issubclass(cls, tomo.objects.SetOfTomograms):
+            view = DeepFinderTomoView(obj)
 
         # deepfinder_args = ''
         # if self.tomogram.get() != None:
@@ -30,4 +40,26 @@ class DeepFinderViewer(pwviewer.Viewer):
         #         if fname_tomo != '':  # if a tomo is linked to segmentation, also display tomo
         #             deepfinder_args += ' -t ' + fname_tomo
 
-        pass
+        view._env = env
+        return [view]
+
+class DeepFinderTomoView(pwviewer.CommandView):
+    """ Wrapper to visualize different type of objects with the DeepFinderDisplay """
+
+    def __init__(self, obj, **kwargs):
+        fname = obj.getFileName()
+        #deepfinder_args = '-t ' + fname
+        #Plugin.runDeepFinder(self, 'display', deepfinder_args)
+        pwviewer.CommandView.__init__(self, Plugin.getDeepFinderCmd('display') + ' -t' + fname)
+
+class DeepFinderSetOfTomogramsView(pwviewer.CommandView):
+    """ Wrapper to visualize set of tomograms with DeepFinderDisplay """
+
+    def __init__(self, set, **kwargs):
+        for item in set:
+            fname = item.getFileName()
+            #deepfinder_args = '-t ' + fname
+            #Plugin.runDeepFinder(self, 'display', deepfinder_args) # /!\ Can cause OOM if too many items
+            pwviewer.CommandView.__init__(self, Plugin.getDeepFinderCmd('display') + ' -t' + fname)
+
+
