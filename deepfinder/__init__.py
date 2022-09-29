a17f0c661c019e1d48e63c2cd061db2eb055ab2a
JorMaister <jorgejjs86@gmail.com>
2022-09-29 11:20:43 +0200

    Update installation requirements II

--- deepfinder/__init__.py
+++ deepfinder/__init__.py
@@ -102,6 +102,7 @@ class Plugin(pwem.Plugin):
         # installationCmd += 'pip install -r requirements_gpu.txt && '  # for GPU usage should be requirements_gpu.txt
         installationCmd += 'pip install tensorflow-gpu==2.6.0 && '
         installationCmd += 'pip install keras==2.6 && '
+        installationCmd += 'pip install mrcfile && '
         installationCmd += 'pip install h5py==3.1.0 && '
         installationCmd += 'pip install lxml==4.3.4 && '
         installationCmd += 'pip install matplotlib==3.1.0 && '
